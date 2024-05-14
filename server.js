47d2c3d42ba5bb1c0db445ddc0b557166a353c75
Besfort Ramadani <71851042+BesRam@users.noreply.github.com>
2024-05-14 16:08:43 +0200

    change text of output

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello FS2024 DevOps Course!!!');
+    res.send('Hello Ramadbes, you are enrolled in the DevOps Course FS2024!!');
 });
 
 app.listen(PORT, HOST);
